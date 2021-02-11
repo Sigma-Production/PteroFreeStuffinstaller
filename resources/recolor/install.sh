8334f31f7dc71128fd3a245e37ea1b339afe5d8a
Jamie L <51766327+DeveloperNeon@users.noreply.github.com>
2021-02-11 11:32:20 -0600

    Create install.sh

---
+++ resources/recolor/install.sh
@@ -0,0 +1,24 @@
+php /var/www/pterodactyl/artisan down
+cd /var/www/pterodactyl/resources/scripts
+rm -rf main.css
+echo "@import url('https://oreokitten.github.io/latest/Recolor/user.css')" >> /var/www/pterodactyl/resources/scripts/main.css
+rm -rf index.tsx
+wget https://raw.githubusercontent.com/DeveloperNeon/ptero-1.0-theme-install/main/resources/index.tsx
+cd ../..
+php artisan view:clear
+php artisan cache:clear
+clear
+echo "User panel theme has been added."
+echo "Installing admin theme..."
+cd /var/www/pterodactyl/resources/views/layouts/
+rm -rf admin.blade.php
+wget https://raw.githubusercontent.com/DeveloperNeon/ptero-1.0-theme-install/main/resources/Recolor/admin.blade.php
+npm install yarn -g
+yarn install
+yarn build:production
+cd ../../..
+php /var/www/pterodactyl/artisan up
+php /var/www/pterodactyl/artisan view:clear
+php /var/www/pterodactyl/artisan cache:clear
+clear
+echo "Admin theme install complete, Make sure to shift + refresh your page!"
