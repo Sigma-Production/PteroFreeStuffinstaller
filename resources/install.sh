0e51e685d01456277c91afceede4c4d5807f1a92
Jamie L <51766327+DeveloperNeon@users.noreply.github.com>
2021-02-11 12:42:55 -0600

    Create install.sh

---
+++ resources/install.sh
@@ -0,0 +1,56 @@
+echo "1. Dracula - Panel Version: 1.2.0, 1.2.1, and 1.2.2\n"
+echo "2. Dark-N-Purple - Panel Version: 1.2.2\n"
+echo "3. Enola - Panel Version: 1.2.0, 1.2.1, and 1.2.2\n"
+echo "4. Recolor - Panel Version: 1.2.0, 1.2.1, and 1.2.2\n"
+echo "5. Twilight - Panel Version: 1.1.3, 1.2.0, 1.2.1, and 1.2.2\n"
+echo "6. Cancel\n"
+echo "\n\nScreenshots of the themes can be found at: https://github.com/OreoKitten/PteroThemes\n"
+read -p "Please enter a number: " choice
+if [ $choice == "1" ]
+    then
+    echo "Installing Dracula..."
+    bash <(curl -sSL https://raw.githubusercontent.com/WeLikeToCodeStuff/ptero-1.0-theme-install/main/resources/dracula/install.sh)
+    else
+    echo Inalid Option
+    exit 0
+fi
+if [ $choice == "2" ]
+    then
+    echo "Installing Dark-N-Purple..."
+    bash <(curl -sSL https://raw.githubusercontent.com/WeLikeToCodeStuff/ptero-1.0-theme-install/main/resources/dark-n-purple/install.sh)
+    else
+    echo Inalid Option
+    exit 0
+fi
+if [ $choice == "3" ]
+    then
+    echo "Installing enola..."
+    bash <(curl -sSL https://raw.githubusercontent.com/WeLikeToCodeStuff/ptero-1.0-theme-install/main/resources/enola/install.sh)
+    else
+    echo Inalid Option
+    exit 0
+fi
+if [ $choice == "4" ]
+    then
+    echo "Installing Recolor..."
+    bash <(curl -sSL https://raw.githubusercontent.com/WeLikeToCodeStuff/ptero-1.0-theme-install/main/resources/recolor/install.sh)
+    else
+    echo Inalid Option
+    exit 0
+fi
+if [ $choice == "5" ]
+    then
+    echo "Installing Twilight..."
+    bash <(curl -sSL https://raw.githubusercontent.com/WeLikeToCodeStuff/ptero-1.0-theme-install/main/resources/twilight/install.sh)
+    else
+    echo Inalid Option
+    exit 0
+fi
+if [ $choice == "6" ]
+    then
+    echo "Canceling..."
+    exit 0
+    else
+    echo Inalid Option
+    exit 0
+fi
