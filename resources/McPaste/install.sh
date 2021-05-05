58ec5b84b1c3425555707f8589094ae4802f539a
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2021-05-05 11:09:30 +0200

    Update install.sh

--- resources/McPaste/install.sh
+++ resources/McPaste/install.sh
@@ -1,7 +1,7 @@
 php /var/www/pterodactyl/artisan down
 cd /var/www/pterodactyl/resources/scripts/components/server
 rm -rf ServerConsole.tsx
-wget https://raw.githubusercontent.com/finnie2006/ptero-1.0-theme-install/main/resources/dark-n-purple/McPaste.tsx
+wget https://raw.githubusercontent.com/finnie2006/MCpastepteroinstaller/main/resources/McPaste/McPaste.tsx
 wget https://raw.githubusercontent.com/finnie2006/MCpastepteroinstaller/main/resources/McPaste/ServerConsole.tsx
 clear
 npm i -g yarn
@@ -10,4 +10,4 @@ yarn install
 yarn run build:production
 php /var/www/pterodactyl/artisan up
 clear
-echo "PasteBin Installed"
+echo "McPaste Installed"
