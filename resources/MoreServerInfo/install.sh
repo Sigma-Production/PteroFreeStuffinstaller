de1f47ccbf4b8a2466f280e10ac255054d979f41
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2021-05-05 13:04:10 +0200

    Create install.sh

---
+++ resources/MoreServerInfo/install.sh
@@ -0,0 +1,10 @@
+php /var/www/pterodactyl/artisan down
+cd /var/www/pterodactyl/resources/views/admin/servers
+rm -rf index.blade.php
+wget 
+wget 
+clear
+yarn run build:production
+php /var/www/pterodactyl/artisan up
+clear
+echo "MoreServerInfo Installed"
