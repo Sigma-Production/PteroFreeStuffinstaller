32fa1be0d89b2f33fe41025db70b38caef4e09e2
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2021-05-05 13:05:27 +0200

    Update install.sh

--- resources/MoreServerInfo/install.sh
+++ resources/MoreServerInfo/install.sh
@@ -1,8 +1,7 @@
 php /var/www/pterodactyl/artisan down
 cd /var/www/pterodactyl/resources/views/admin/servers
 rm -rf index.blade.php
-wget 
-wget 
+wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/MoreServerInfo/index.blade.php
 clear
 yarn run build:production
 php /var/www/pterodactyl/artisan up
