71238793a6206a279c0e4ccddf612881559e149e
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2021-05-05 14:13:58 +0200

    Update install.sh

--- resources/McPaste/install.sh
+++ resources/McPaste/install.sh
@@ -1,8 +1,8 @@
 php /var/www/pterodactyl/artisan down
 cd /var/www/pterodactyl/resources/scripts/components/server
 rm -rf ServerConsole.tsx
-wget https://raw.githubusercontent.com/finnie2006/MCpastepteroinstaller/main/resources/McPaste/McPaste.tsx
-wget https://raw.githubusercontent.com/finnie2006/MCpastepteroinstaller/main/resources/McPaste/ServerConsole.tsx
+wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/McPaste/McPaste.tsx
+wget hhttps://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/McPaste/ServerConsole.tsx
 clear
 npm i -g yarn
 cd /var/www/pterodactyl
