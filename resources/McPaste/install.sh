a296141a06c0d9839d287989af4fcfe4cbe09765
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2021-05-05 14:14:11 +0200

    Update install.sh

--- resources/McPaste/install.sh
+++ resources/McPaste/install.sh
@@ -3,11 +3,9 @@ cd /var/www/pterodactyl/resources/scripts/components/server
 rm -rf ServerConsole.tsx
 wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/McPaste/McPaste.tsx
 wget hhttps://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/McPaste/ServerConsole.tsx
-clear
 npm i -g yarn
 cd /var/www/pterodactyl
 yarn install
 yarn run build:production
 php /var/www/pterodactyl/artisan up
-clear
 echo "McPaste Installed"
