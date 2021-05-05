f6308da78735424c5cc179c7a5ce156a21886dfd
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2021-05-05 13:57:25 +0200

    Update script.sh

--- resources/script.sh
+++ resources/script.sh
@@ -1,28 +1,46 @@
-echo "1. Mcpaste Install - Panel Version: 1.x.x"
-echo "2. MoreServerInfo - Panel Version: 1.x.x"
-echo "3. McPaste Uninstall"
-echo "4. MoreServerInfo Uninstall"
-echo "6. Cancel"
+echo "1. Dracula - Panel Version: 1.2.0, 1.2.1, and 1.2.2"
+echo "2. Dark-N-Purple - Panel Version: 1.2.2"
+echo "3. Enola - Panel Version: 1.2.0, 1.2.1, and 1.2.2"
+echo "4. Recolor - Panel Version: 1.2.0, 1.2.1, and 1.2.2"
+echo "5. Twilight - Panel Version: 1.1.3, 1.2.0, 1.2.1, and 1.2.2"
+echo "6. Uninstall"
+echo "7. Cancel"
 echo ""
 echo ""
+echo "Screenshots of the themes can be found at: https://github.com/OreoKitten/PteroThemes"
 read -p "Please enter a number: " choice
 if [ $choice == "1" ]
     then
-    echo "Installing McPaste..."
-    bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/McPaste/install.sh)
+    echo "Installing Dracula..."
+    bash <(curl -sSL https://raw.githubusercontent.com/WeLikeToCodeStuff/ptero-1.0-theme-install/main/resources/dracula/install.sh)
 fi
 if [ $choice == "2" ]
     then
-    echo "Installing MoreServerInfo..."
-    bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/MoreServerInfo/install.sh)
+    echo "Installing Dark-N-Purple..."
+    bash <(curl -sSL https://raw.githubusercontent.com/WeLikeToCodeStuff/ptero-1.0-theme-install/main/resources/dark-n-purple/install.sh)
 fi
 if [ $choice == "3" ]
     then
-    echo "Removing McPaste..."
-    bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/uninstall/uninstall.sh)
+    echo "Installing enola..."
+    bash <(curl -sSL https://raw.githubusercontent.com/WeLikeToCodeStuff/ptero-1.0-theme-install/main/resources/enola/install.sh)
 fi
 if [ $choice == "4" ]
     then
-    echo "Removing MoreServerInfo..."
-    bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/uninstall/uninstall2.sh)
+    echo "Installing Recolor..."
+    bash <(curl -sSL https://raw.githubusercontent.com/WeLikeToCodeStuff/ptero-1.0-theme-install/main/resources/recolor/install.sh)
+fi
+if [ $choice == "5" ]
+    then
+    echo "Installing Twilight..."
+    bash <(curl -sSL https://raw.githubusercontent.com/WeLikeToCodeStuff/ptero-1.0-theme-install/main/resources/twilight/install.sh)
+fi
+if [ $choice == "6" ]
+    then
+    echo "Uninstalling..."
+    bash <(curl -sSL https://raw.githubusercontent.com/WeLikeToCodeStuff/ptero-1.0-theme-install/main/resources/uninstall/uninstall.sh)
+fi
+if [ $choice == "7" ]
+    then
+    echo "Canceling..."
+    exit 0
 fi
