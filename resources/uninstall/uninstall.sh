5ce9532b60266b992ef109b928e5634488a7d1c6
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2021-05-05 11:06:28 +0200

    Update uninstall.sh

--- resources/uninstall/uninstall.sh
+++ resources/uninstall/uninstall.sh
@@ -1,8 +1,8 @@
 php /var/www/pterodactyl/artisan down
 cd /var/www/pterodactyl/resources/scripts/components/server
 rm -rf ServerConsole.tsx
-wget https://raw.githubusercontent.com/finnie2006/ptero-1.0-theme-install/main/resources/dark-n-purple/McPaste.tsx
-wget https://raw.githubusercontent.com/finnie2006/MCpastepteroinstaller/main/resources/McPaste/ServerConsole.tsx
+rm -rf mcpaste.tsx
+wget https://raw.githubusercontent.com/finnie2006/MCpastepteroinstaller/main/resources/uninstall/ServerConsole.tsx
 clear
 npm i -g yarn
 cd /var/www/pterodactyl
