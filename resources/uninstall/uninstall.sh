6f26342f97a4ed4f0b2f3f834c56dd78ef92951a
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2021-05-05 11:07:18 +0200

    Update uninstall.sh

--- resources/uninstall/uninstall.sh
+++ resources/uninstall/uninstall.sh
@@ -1,7 +1,7 @@
 php /var/www/pterodactyl/artisan down
 cd /var/www/pterodactyl/resources/scripts/components/server
 rm -rf ServerConsole.tsx
-rm -rf mcpaste.tsx
+rm -rf McPaste.tsx
 wget https://raw.githubusercontent.com/finnie2006/MCpastepteroinstaller/main/resources/uninstall/ServerConsole.tsx
 clear
 npm i -g yarn
