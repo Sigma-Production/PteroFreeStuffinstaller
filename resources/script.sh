a7535fa19a94118d63d3402d93fa50b50b1b4765
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2021-05-05 14:56:46 +0200

    Update script.sh

--- resources/script.sh
+++ resources/script.sh
@@ -2,8 +2,8 @@ echo "1. McPaste Install"
 echo "2. MoreServerInfo Install"
 echo "3. Remove Mcpaste"
 echo "4. Remove MoreServerInfo"
-echo "5. Cancel"
-echo ""
+echo "5. Install Dark N Red Theme"
+echo "6. Cancel"
 echo ""
 echo ""
 read -p "Please enter a number: " choice
@@ -26,6 +26,12 @@ if [ $choice == "4" ]
     then
     echo "Removing MoreServerInfo..."
     bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/uninstall/uninstall2.sh)
+fi
+if [ $choice == "4" ]
+    then
+    echo "Installing Dark N Red Theme..."
+    bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/DarkNRed/install.sh)
+
 
 fi
 if [ $choice == "5" ]
