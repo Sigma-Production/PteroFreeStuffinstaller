76c355b2a2ce4b1119221953b1d15ce0ea93ed7a
TwixGamer00 <64733980+TwixGamer00@users.noreply.github.com>
2021-05-06 09:35:09 +0200

    Update script.sh

--- resources/script.sh
+++ resources/script.sh
@@ -2,11 +2,9 @@ echo "1. McPaste Install"
 echo "2. MoreServerInfo Install"
 echo "3. Remove Mcpaste"
 echo "4. Remove MoreServerInfo"
-echo "5. Install Dark N Red Theme"
-echo "6. Install Dark N Green Theme"
-echo "7. Install Dark N Purple Theme"
-echo "8. Install Minecraft Theme"
-echo "9 Close"
+echo "5. Install Dark N Color Theme (Yellow, Purple, Green or Red)"
+echo "6. Install Minecraft Theme"
+echo "7. Close / Cancel"
 echo ""
 read -p "Please enter a number: " choice
 if [ $choice == "1" ]
@@ -31,22 +29,9 @@ if [ $choice == "4" ]
 fi
 if [ $choice == "5" ]
     then
-    echo "Installing Dark N Red Theme..."
-    bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/DarkNRed/install.sh)
+    bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/DarkNColor.sh)
 fi
 if [ $choice == "6" ]
-    then
-    echo "Installing Dark N Green Theme..."
-    bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/DarkNGreen/install.sh)
-fi
-if [ $choice == "7" ]
-    then
-    echo "Installing Dark N Purple Theme..."
-    bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/DarkNPurp/install.sh)
-
-
-fi
-if [ $choice == "8" ]
     then
     echo "Installing Minecraft Theme..."
     bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/mcTheme/install.sh)
@@ -54,7 +39,7 @@ if [ $choice == "8" ]
 
 fi
 
-if [ $choice == "9" ]
+if [ $choice == "7" ]
     then
     echo "Canceling..."
     exit 0
