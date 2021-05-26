5a653efc5334fd66de906ea0c625406ce7ffefdd
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2021-05-26 22:12:23 +0200

    Update install.sh

--- resources/statistics/install.sh
+++ resources/statistics/install.sh
@@ -1,12 +1,15 @@
 php /var/www/pterodactyl/artisan down
 cd /var/www/pterodactyl/resources/views/admin
+rm -rf statistics.blade.php
 wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/statistics/statistics.blade.php
 cd /var/www/pterodactyl/resources/views/layouts
-rm -rf statistics.blade.php
+rm -rf admin.blade.php
 wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/statistics/admin.blade.php
 cd /var/www/pterodactyl/app/Http/Controllers/Admin
+rm -rf StatisticsController.php
 wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/statistics/StatisticsController.php
 cd /var/www/pterodactyl/public/themes/pterodactyl/js/admin
+rm -rf statistics.js
 wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/statistics/statistics.js
 clear
 php /var/www/pterodactyl/artisan up
