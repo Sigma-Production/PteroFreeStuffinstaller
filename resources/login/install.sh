632740732bb8b6d3702d9eb3c239f3cca40947df
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2021-05-27 20:25:32 +0200

    Update install.sh

--- resources/login/install.sh
+++ resources/login/install.sh
@@ -1,15 +1,12 @@
 php /var/www/pterodactyl/artisan down
 cd /var/www/pterodactyl/resources/scripts
 rm -rf main.css
-wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/DarkNRed/main.css
+https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/login/main.css
 rm -rf index.tsx
 wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/DarkNRed/index.tsx
-cd /var/www/pterodactyl/resources/views/layouts/
-rm -rf admin.blade.php
-wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/DarkNRed/admin.blade.php
-cd /var/www/pterodactyl/resources/scripts/components/auth
-rm -rf LoginContainer.tsx
-wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/login/LoginContainer.tsx
+cd /var/www/pterodactyl/resources/views/templates/
+rm -rf wrapper.blade.php
+wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/mvb/wrapper.blade.php
 clear
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 apt install -y nodejs
@@ -20,4 +17,4 @@ yarn add @emotion/react
 yarn build:production
 clear
 php /var/www/pterodactyl/artisan up
-echo "Dark N red theme Installed"
+echo "Animated background theme Installed"
