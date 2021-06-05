380f648d91b6ec221b9da971c90e4ddcab907332
RTK23-dev <77203747+RTK23-dev@users.noreply.github.com>
2021-06-05 19:46:12 +0530

    Update install.sh

--- resources/mvb/install.sh
+++ resources/mvb/install.sh
@@ -1,15 +1,15 @@
 php /var/www/pterodactyl/artisan down
 cd /var/www/pterodactyl/resources/scripts
 rm -rf main.css
-wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/mvb/main.css
+wget https://raw.githubusercontent.com/RTK23-dev/PteroFreeStuffinstaller/main/resources/mvb/main.css
 rm -rf index.tsx
-wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/DarkNRed/index.tsx
+wget https://raw.githubusercontent.com/RTK23-dev/PteroFreeStuffinstaller/main/resources/DarkNRed/index.tsx
 cd /var/www/pterodactyl/resources/views/layouts/
 rm -rf admin.blade.php
-wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/DarkNRed/admin.blade.php
+wget https://raw.githubusercontent.com/RTK23-dev/PteroFreeStuffinstaller/main/resources/DarkNRed/admin.blade.php
 cd /var/www/pterodactyl/resources/views/templates/
 rm -rf wrapper.blade.php
-wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/mvb/wrapper.blade.php
+wget https://raw.githubusercontent.com/RTK23-dev/PteroFreeStuffinstaller/main/resources/mvb/wrapper.blade.php
 clear
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 apt install -y nodejs
