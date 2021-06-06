61e961317fece3db79616e355a50eb3c24f7ba22
RTK23-dev <77203747+RTK23-dev@users.noreply.github.com>
2021-06-06 09:19:07 +0530

    Update install.sh

--- resources/Navy/install.sh
+++ resources/Navy/install.sh
@@ -2,7 +2,7 @@
 php /var/www/pterodactyl/artisan down
 cd /var/www/pterodactyl/resources/scripts
 rm -rf main.css
-wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/Navy/main.css
+wget https://raw.githubusercontent.com/RTK23-dev/PteroFreeStuffinstaller/main/resources/Navy/main.css
 clear
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 apt install -y nodejs
@@ -13,4 +13,4 @@ yarn add @emotion/react
 yarn build:production
 clear
 php /var/www/pterodactyl/artisan up
-echo "Navy and Dark theme Installed"
+echo "Navy theme Installed"
