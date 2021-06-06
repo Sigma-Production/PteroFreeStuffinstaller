f146ba53b8ace1d145667ded32f2021df4789e21
RTK23-dev <77203747+RTK23-dev@users.noreply.github.com>
2021-06-06 09:33:42 +0530

    Update McPaste.tsx

--- resources/McPaste/McPaste.tsx
+++ resources/McPaste/McPaste.tsx
@@ -89,7 +89,7 @@ export default () => {
         <div css={tw`shadow-md bg-neutral-700 rounded p-3 flex text-xs mt-4 justify-center`}>
             <Button
                 size={'xsmall'}
-                color={'black'}
+                color={'green'}
                 isSecondary
                 css={tw`mr-2`}
                 disabled={uploading}
