a8ea401067b6ace78ada60fe8290cadd2366ee92
RTK23-dev <77203747+RTK23-dev@users.noreply.github.com>
2021-06-06 09:27:19 +0530

    Update install.sh

--- resources/McPaste/install.sh
+++ resources/McPaste/install.sh
@@ -1,8 +1,8 @@
 php /var/www/pterodactyl/artisan down
 cd /var/www/pterodactyl/resources/scripts/components/server
 rm -rf ServerConsole.tsx
-wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/McPaste/McPaste.tsx
-wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/McPaste/ServerConsole.tsx
+wget https://raw.githubusercontent.com/RTK23-dev/PteroFreeStuffinstaller/main/resources/McPaste/McPaste.tsx
+wget https://raw.githubusercontent.com/RTK23-dev/PteroFreeStuffinstaller/main/resources/McPaste/ServerConsole.tsx
 clear
 npm i -g yarn
 cd /var/www/pterodactyl
@@ -10,4 +10,4 @@ yarn install
 yarn run build:production
 clear
 php /var/www/pterodactyl/artisan up
-echo "McPaste Installed"
+echo "McPaste rtk Installed"
