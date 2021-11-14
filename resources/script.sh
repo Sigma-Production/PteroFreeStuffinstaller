17eba6daa94fd060048663e6c72b65751ba5030c
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2021-11-14 16:57:07 +0100

    New installer script
    New script

--- resources/script.sh
+++ resources/script.sh
@@ -1,48 +1,141 @@
-echo "1. MoreServerInfo Install"
-echo "2. Remove MoreServerInfo"
-echo "3. Install Dark N Color Theme (Yellow, Purple, Green or Red)"
-echo "4. Install Minecraft Theme"
-echo "5. Install Animated Background Theme"
-echo "6. Install Statistics page"
-echo "7. Install phpMyAdmin button"
-echo "8  Close / Cancel"
-read -p "Please enter a number: " choice
-if [ $choice == "1" ]
-    then
-    echo "Installing MoreServerInfo..."
-    bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/MoreServerInfo/install.sh)
+#!/bin/bash
+
+set -e
+
+
+SCRIPT_VERSION="v1.8.5"
+
+
+print_brake() {
+  for ((n = 0; n < $1; n++)); do
+    echo -n "#"
+  done
+  echo ""
+}
+
+hyperlink() {
+  echo -e "\e]8;;${1}\a${1}\e]8;;\a"
+}
+
+#### Colors ####
+
+GREEN="\e[0;92m"
+YELLOW="\033[1;33m"
+reset="\e[0m"
+red='\033[0;31m'
+
+error() {
+  COLOR_RED='\033[0;31m'
+  COLOR_NC='\033[0m'
+
+  echo ""
+  echo -e "* ${COLOR_RED}ERROR${COLOR_NC}: $1"
+  echo ""
+}
+
+
+#### Check Sudo ####
+
+if [[ $EUID -ne 0 ]]; then
+  echo "* This script must be executed with root privileges (sudo)." 1>&2
+  exit 1
 fi
-if [ $choice == "2" ]
-    then
-    echo "Removing MoreServerInfo..."
-    bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/uninstall/uninstall2.sh)
+
+
+#### Check Curl ####
+
+if ! [ -x "$(command -v curl)" ]; then
+  echo "* curl is required in order for this script to work."
+  echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
+  exit 1
 fi
-if [ $choice == "3" ]
-    then
-    bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/DarkNColor.sh)
-fi
-if [ $choice == "4" ]
-    then
-    echo "Installing Minecraft Theme..."
-    bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/mcTheme/install.sh)
-fi
-if [ $choice == "5" ]
-    then
-    echo "Installing Animated Background Theme..."
-    bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/mvb/install.sh)
-fi
-if [ $choice == "6" ]
-    then
-    echo "Installing Statistics page..."
-    bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/statistics/install.sh)
-fi
-if [ $choice == "7" ]
-    then
-    echo "Installing phpMyAdmin button..."
-    bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/phpmyadmin/install.sh)
-fi
-if [ $choice == "8" ]
-    then
-    echo "Canceling..."
-    exit 0
-fi
+
+cancel() {
+echo
+echo -e "* ${red}Installation Canceled!${reset}"
+done=true
+exit 1
+}
+
+done=false
+
+echo
+print_brake 70
+echo "* Pterodactyl-Freestuff-Installer Script @ $SCRIPT_VERSION"
+echo
+echo "* Copyright (C) 2021 - 2021."
+echo "* https://github.com/finnie2006/PteroFreeStuffinstaller"
+echo
+echo "* This script is not associated with the official Pterodactyl Project."
+print_brake 70
+echo
+
+DarkNColor() {
+bash clear
+bash <(curl -s https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/DarkNColor.sh)
+}
+
+MoreServerInfo() {
+bash <(curl -s https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/MoreServerInfo/install.sh)
+}
+
+MinecraftTheme() {
+ bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/mcTheme/install.sh)
+}
+
+mvbtheme() {
+ bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/mvb/install.sh)
+}
+
+phpmyadmin() {
+ bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/phpmyadmin/install.sh)
+  }
+
+ThemRemover() {
+ bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/uninstall/uninstall.sh)
+  }
+
+
+
+while [ "$done" == false ]; do
+  options=(
+    "Install DarkNColor"
+    "Install MoreServerInfo"
+    "Install MinecraftTheme"
+    "Install Animated Background theme"
+    "Install phpmyadminbutton addon"
+    "ThemeRemover"
+    
+    
+    "Cancel Installation"
+  )
+  
+  actions=(
+    "DarkNColor"
+    "MoreServerInfo"
+    "MinecraftTheme"
+    "mvbtheme"
+    "phpmyadmin"
+    "ThemeRemover"
+    
+    
+    "cancel"
+  )
+  
+  echo "* Which theme do you want to install?"
+  echo
+  
+  for i in "${!options[@]}"; do
+    echo "[$i] ${options[$i]}"
+  done
+  
+  echo
+  echo -n "* Input 0-$((${#actions[@]} - 1)): "
+  read -r action
+  
+  [ -z "$action" ] && error "Input is required" && continue
+  
+  valid_input=("$(for ((i = 0; i <= ${#actions[@]} - 1; i += 1)); do echo "${i}"; done)")
+  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Invalid option"
+  [[ " ${valid_input[*]} " =~ ${action} ]] && done=true && eval "${actions[$action]}"
+done
