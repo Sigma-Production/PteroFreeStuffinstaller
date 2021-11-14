a3f089cf2d40c2b71a27606e15819fcd0da00214
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2021-11-14 14:49:32 +0100

    Update uninstall.sh

--- resources/uninstall/uninstall.sh
+++ resources/uninstall/uninstall.sh
@@ -1,13 +1,9 @@
 php /var/www/pterodactyl/artisan down
-cd /var/www/pterodactyl/resources/scripts/components/server
-rm -rf ServerConsole.tsx
-rm -rf McPaste.tsx
-wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/uninstall/ServerConsole.tsx
+cd /var/www/pterodactyl/resources/scripts/
+rm main.css
+rm index.tsx
+wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/uninstall/index.tsx
 clear
-npm i -g yarn
-cd /var/www/pterodactyl
-yarn install
-yarn run build:production
 php /var/www/pterodactyl/artisan up
 clear
-echo "Mcpaste Removed"
+echo "Removed Theme"
