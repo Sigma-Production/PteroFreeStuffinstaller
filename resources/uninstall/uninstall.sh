1aed0f78a058a7db951a0e513fc8d6aaebf723e9
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2021-11-14 15:00:12 +0100

    Update uninstall.sh

--- resources/uninstall/uninstall.sh
+++ resources/uninstall/uninstall.sh
@@ -3,6 +3,9 @@ cd /var/www/pterodactyl/resources/scripts/
 rm main.css
 rm index.tsx
 wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/uninstall/index.tsx
+cd /var/www/pterodactyl/resources/views/templates
+rm wrapper.blade.php
+wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/uninstall/wrapper.blade.php
 clear
 php /var/www/pterodactyl/artisan up
 clear
