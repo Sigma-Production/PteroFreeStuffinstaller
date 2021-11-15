d5851165c55b85c7d3b62212b4fc83574e6b0eee
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2021-11-15 21:36:36 +0100

    Update install.sh

--- resources/DarkNRed/install.sh
+++ resources/DarkNRed/install.sh
@@ -1,3 +1,24 @@
+#### Panel Backup ####
+backup() {
+echo
+print_brake 32
+echo -e "* ${GREEN}Performing security backup...${reset}"
+print_brake 32
+if [ -f "/var/www/pterodactyl/PanelBackup/PanelBackup.zip" ]; then
+echo
+print_brake 45
+echo -e "* ${GREEN}There is already a backup, skipping step...${reset}"
+print_brake 45
+echo
+else
+cd /var/www/pterodactyl
+mkdir -p PanelBackup
+zip -r PanelBackup.zip app config public resources routes storage database .env tailwind.config.js
+mv PanelBackup.zip PanelBackup
+fi
+}
+
+### Install theme ###
 php /var/www/pterodactyl/artisan down
 cd /var/www/pterodactyl/resources/scripts
 rm -rf main.css
