5d36f0acbd5e5016d45818d209e802d30524abd2
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2021-11-15 21:42:32 +0100

    Update install.sh

--- resources/Aqua/install.sh
+++ resources/Aqua/install.sh
@@ -1,3 +1,24 @@
+
+#### Panel Backup ####
+backup() {
+echo
+print_brake 32
+echo -e "* ${GREEN}Performing security backup...${reset}"
+print_brake 32
+if [ -f "/var/www/pterodactyl/PanelBackup/PanelBackup.zip" ]; then
+echo
+print_brake 45
+echo -e "* ${GREEN}There is already a backup, skipping step...${reset}"
+print_brake 45
+echo
+else
+cd /var/www/pterodactyl
+mkdir -p PanelBackup
+zip -r PanelBackup.zip app config public resources routes storage database .env tailwind.config.js
+mv PanelBackup.zip PanelBackup
+fi
+}
+
 set -e
 php /var/www/pterodactyl/artisan down
 cd /var/www/pterodactyl/resources/scripts
@@ -6,7 +27,7 @@ wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/r
 rm -rf index.tsx
 wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/DarkNRed/index.tsx
 clear
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 apt install -y nodejs
 npm i -g yarn
 cd /var/www/pterodactyl
