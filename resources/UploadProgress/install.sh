a357c1c51a9f18c5043c646858e9265bb2964665
BuildTools <32767771+beastksoepic@users.noreply.github.com>
2021-12-18 21:41:34 +0700

    fix install.sh

--- resources/UploadProgress/install.sh
+++ resources/UploadProgress/install.sh
@@ -1,4 +1,5 @@
 cd /var/www/pterodactyl/resources/scripts/components/server/files/
+rm -rf UploadButton.tsx
 wget https://raw.githubusercontent.com/beastksoepic/PteroFreeStuffinstaller/main/resources/UploadProgress/UploadButton.tsx
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
