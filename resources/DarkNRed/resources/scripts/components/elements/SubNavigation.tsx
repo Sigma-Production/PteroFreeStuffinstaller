7e00eea0733f2f658d27610e1a8fced838bb65d2
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2021-12-19 11:39:46 +0100

    DarkNRed V2

---
+++ resources/DarkNRed/resources/scripts/components/elements/SubNavigation.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components/macro';
+import tw, { theme } from 'twin.macro';
+
+const SubNavigation = styled.div`
+    ${tw`w-full bg-theme-main-2 shadow overflow-x-auto`};
+
+    & > div {
+        ${tw`flex items-center text-sm mx-auto px-2`};
+        max-width: 1200px;
+
+        & > a, & > div {
+            ${tw`inline-block py-3 px-4 text-neutral-300 no-underline whitespace-nowrap transition-all duration-150`};
+
+            &:not(:first-of-type) {
+                ${tw`ml-2`};
+            }
+
+            &:hover {
+                ${tw`text-neutral-100`};
+            }
+
+            &:active, &.active {
+                ${tw`text-neutral-100`};
+                box-shadow: inset 0 -2px #DC143C;
+            }
+        }
+    }
+`;
+
+export default SubNavigation;
