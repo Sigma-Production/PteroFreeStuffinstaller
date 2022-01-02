1c5dac1e3c64303ec07e62208d24be07b40ed970
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2022-01-02 13:24:43 +0100

    Update NavigationBar.tsx

--- resources/DarkNBlue/resources/scripts/components/NavigationBar.tsx
+++ resources/DarkNBlue/resources/scripts/components/NavigationBar.tsx
@@ -38,7 +38,7 @@ const RightNavigation = styled.div`
         }
         
         &:active, &:hover, &.active {
-            box-shadow: inset 0 -2px #dc143c;
+            box-shadow: inset 0 -2px #3205fa;
         }
     }
 `;
