281aab1488026b156867a73075c55e9c78c4f010
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2022-01-08 23:20:58 +0100

    Update install.sh
    Added script

--- resources/login/install.sh
+++ resources/login/install.sh
@@ -1,20 +1,17 @@
 php /var/www/pterodactyl/artisan down
-cd /var/www/pterodactyl/resources/scripts
-rm -rf main.css
-https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/login/main.css
-rm -rf index.tsx
-wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/DarkNRed/index.tsx
-cd /var/www/pterodactyl/resources/views/templates/
-rm -rf wrapper.blade.php
-https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/login/wrapper.blade.php
+cd /var/www/pterodactyl/resources/scripts/assets/css/
+rm GlobalStylesheet.ts
+wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/V2/resources/login/resources/scripts/assets/css/GlobalStylesheet.ts
+cd /var/www/pterodactyl/resources/scripts/components/auth/
+rm LoginFormContainer.tsx
+wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/V2/resources/login/resources/scripts/components/auth/LoginFormContainer.tsx
 clear
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-apt install -y nodejs
+cd /var/www/pterodactyl
+yarn add react-tsparticles
 npm i -g yarn
 cd /var/www/pterodactyl
 yarn install
-yarn add @emotion/react
-yarn build:production
+yarn run build:production
 clear
 php /var/www/pterodactyl/artisan up
-echo "Animated background theme Installed"
+echo "Login animation Installed"
