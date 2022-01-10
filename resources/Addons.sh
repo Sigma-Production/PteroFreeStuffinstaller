7c4f85e68ac63e0a7f2ac6fab6f4d1e4d55e87e2
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2022-01-10 07:43:36 +0100

    Update Addons.sh
    Add statistics add on back

--- resources/Addons.sh
+++ resources/Addons.sh
@@ -3,7 +3,8 @@ echo "1. Serperate Kill-Button"
 echo "2. McPaste"
 echo "3. MoreServerInfo"
 echo "4. pma Button"
-echo "5. Close / Cancel"
+echo "5. Statistics Addon"
+echo "6. Close / Cancel"
 echo ""
 read -p "Please enter a number: " choice
 if [ $choice == "1" ]
@@ -26,8 +27,13 @@ if [ $choice == "4" ]
     echo "Installing Phpmyadmin button..."
     bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/V2/resources/phpmyadmin/install.sh)
 fi
-
 if [ $choice == "5" ]
+    then
+    echo "Installing 0.7 Addon Statistics ..."
+    bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/V2/resources/statistics/install.sh)
+fi
+
+if [ $choice == "6" ]
     then
     echo "Canceling..."
     exit 0
