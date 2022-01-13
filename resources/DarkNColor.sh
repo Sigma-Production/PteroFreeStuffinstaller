3e74ee478b55ba753e4c897ed28667ad3947f15b
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2022-01-13 14:11:59 +0100

    Update DarkNColor.sh

--- resources/DarkNColor.sh
+++ resources/DarkNColor.sh
@@ -1,9 +1,10 @@
 echo ""
 echo "1. DarkNRed"
-echo "2. DarkNYellow"
-echo "3. DarkNGreen"
-echo "4. DarkNPurple"
-echo "5. Close / Cancel"
+echo "2. DarkNBlue"
+echo "3. DarkNYellow"
+echo "4. DarkNGreen"
+echo "5. DarkNPurple"
+echo "6. Close / Cancel"
 echo ""
 read -p "Please enter a number: " choice
 if [ $choice == "1" ]
@@ -13,21 +14,29 @@ if [ $choice == "1" ]
 fi
 if [ $choice == "2" ]
     then
+    echo "Installing DarkNBlue Theme..."
+    bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/V2/resources/DarkNBlue/install.sh)
+fi
+if [ $choice == "3" ]
+    then
     echo "Installing DarkNYellow Theme..."
     bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/V2/resources/DarkNYellow/install.sh)
 fi
-if [ $choice == "3" ]
+if [ $choice == "4" ]
     then
     echo "Installing DarkNGreen Theme..."
     bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/V2/resources/DarkNGreen/install.sh)
 fi
-if [ $choice == "4" ]
+if [ $choice == "5" ]
     then
     echo "Installing DarkNPurple Theme..."
     bash <(curl -sSL https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/V2/resources/DarkNPurple/install.sh)
 fi
 
-if [ $choice == "5" ]
+
+
+
+if [ $choice == "6" ]
     then
     echo "Canceling..."
     exit 0
