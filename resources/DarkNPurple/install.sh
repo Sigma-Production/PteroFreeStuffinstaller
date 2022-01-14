bfd4195e3eb13003804165608ce11f722a7af08d
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2022-01-14 22:49:29 +0100

    Update install.sh

--- resources/DarkNPurple/install.sh
+++ resources/DarkNPurple/install.sh
@@ -2,7 +2,23 @@ php /var/www/pterodactyl/artisan down
 cd /var/www/pterodactyl
 DIR="/var/www/pterodactyl/backup"
 if [ -d "$DIR" ]; then
-   echo "'$DIR' There already is a backup going furthur ..."
+echo -n "$DIR' There already is a backup do you want to create a new one? y/n "
+read answer
+
+# if echo "$answer" | grep -iq "^y" ;then
+
+if [ "$answer" != "${answer#[Yy]}" ] ;then # this grammar (the #[] operator) means that the variable $answer where any Y or y in 1st position will be dropped if they exist.
+    echo Yes
+rm -r backup/*
+mkdir -p backup/{resources,public}
+   cp -r resources/* backup/resources/
+   cp -r public/* backup/public/
+   cp tailwind.config.js backup/
+   echo "Created Backup going furthur"
+else
+    echo No
+fi
+
 else
    echo "No backup found making one"
    mkdir -p backup/{resources,public}
@@ -11,15 +27,27 @@ else
    cp tailwind.config.js backup/
    echo "Created Backup going furthur"
 fi
-cd /var/www/pterodactyl
-sudo curl https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/V2/resources/DarkNPurple/DarkNPurple.tar.gz  | sudo tar -xz
+
+sudo curl https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/V2/resources/DarkNPurple/DarkNPurple.tar.gz | sudo tar -xz
 #clear
 cd /var/www/pterodactyl
-if ! command -v node -v &> /dev/null
-then
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-apt-get install -y nodejs
+
+if [ `which yum` ]; then
+  if ! command -v node -v &> /dev/null
+  then
+    curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
+    yum install nodejs
+  fi
+elif [ `which apt` ]; then
+  if ! command -v node -v &> /dev/null
+  then
+    curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+    apt-get install -y nodejs
+  fi
+else
+   echo "Your OS is unsupported"
 fi
+
 if ! command -v yarn -v &> /dev/null
 then
     npm i -g yarn
