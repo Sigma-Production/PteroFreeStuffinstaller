2d9fdaab4ba03dff65925e530fd9097f873f97cf
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2022-01-17 21:40:17 +0100

    Delete resources/uninstall directory

--- resources/uninstall/uninstall2.sh
+++
@@ -1,9 +0,0 @@
-php /var/www/pterodactyl/artisan down
-cd /var/www/pterodactyl/resources/views/admin/servers
-rm -rf index.blade.php
-wget https://raw.githubusercontent.com/finnie2006/PteroFreeStuffinstaller/main/resources/uninstall/index.blade.php
-clear
-yarn run build:production
-php /var/www/pterodactyl/artisan up
-clear
-echo "MoreServerInfo Removed"
