e97c53e06d7b35cb0d22e0f73bb4cbce9653e421
JohnB17 <63297273+JohnB17@users.noreply.github.com>
2022-03-18 10:12:46 -0400

    Update app.php

--- resources/LogoAddon/app.php
+++ resources/LogoAddon/app.php
@@ -9,7 +9,7 @@ return [
     | change this value if you are not maintaining your own internal versions.
     */
 
-    'version' => '1.6.6',
+    'version' => '1.7.0',
 
     /*
     |--------------------------------------------------------------------------
